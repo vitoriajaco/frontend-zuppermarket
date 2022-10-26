0a91c17ef72416cfc9c217ddde75b743001da1ab
vitoriajaco <vitoriajaco@gmail.com>
2022-10-26 16:25:23 -0300

    frontend

---
+++ zuppermarket-main/js/app.js
@@ -0,0 +1,11 @@
+// const SERVER = 'http://localhost/8080';
+
+
+// const formUsuario = document.querySelector('#cadastroUsuario');
+
+// formUsuario.addEventListener('submit', cadastrarUsuario)
+
+// function cadastrarUsuario(Evento) {
+//   Evento.preventDefault();
+//   console.log(Evento)
+// }
